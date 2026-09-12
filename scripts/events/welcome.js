037460591e668b80cfa27a09724b62ccac440152
Ahmed Zihad <gnew46880@gmail.com>
2026-09-12 17:31:34 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -47,7 +47,7 @@ module.exports = {
       }
 
       const threadInfo = await api.getThreadInfo(threadID);
-      const botJoinMedia = await getStreamFromURL("https://files.catbox.moe/souek3.mp4").catch(() => null);
+      const botJoinMedia = await getStreamFromURL("https://i.imgur.com/7wZoMNu.gif").catch(() => null);
 
       return message.send({
         body:
@@ -92,7 +92,7 @@ module.exports = {
       const adderInfo = await api.getUserInfo(adderID).catch(() => ({}));
       const adderName = adderInfo[adderID]?.name || "a Guardian";
 
-      const welcomeGif = await getStreamFromURL("https://files.catbox.moe/oasz8q.mp4").catch(() => null);
+      const welcomeGif = await getStreamFromURL("https://i.imgur.com/fAAXnTN.gif").catch(() => null);
 
       await message.send({
         body:
